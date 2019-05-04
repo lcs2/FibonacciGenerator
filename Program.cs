2461aa1ab830a6eed7eebcbf8ed88f9b9e9d60c2
Łukasz Ziółkowski <encoder1988@gmail.com>
2019-05-04 22:50:18 +0200

    A

--- Program.cs
+++ Program.cs
@@ -8,8 +8,33 @@ namespace ciag_fibo
 {
     class Program
     {
+        static decimal x = 0;
+        static decimal y = 0;
+        static int i;
         static void Main(string[] args)
         {
+            var c = new ConsoleKeyInfo();
+            Start();
+            do
+            {
+                c = Console.ReadKey(true);
+                if ( c.Key == ConsoleKey.R) Start();
+                Console.WriteLine($"{++i, 4}. x: {x, 20}\t y:\t{y, 20}\t, fi = {(decimal)y / x}\t");
+                var z = x + y;
+                x = y;
+                y = z;
+            }
+            while (c.Key != ConsoleKey.Q);
+        }
+        static void Start()
+        {
+            i = 0;
+            Console.WriteLine("podaj 2 liczby:");
+            x = Convert.ToInt64(Console.ReadKey().KeyChar - '0');
+            y = Convert.ToInt64(Console.ReadKey().KeyChar - '0');
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine("Klikaj aby robic powtorzenia (R - reset, Q - wyjscie):");
         }
     }
 }
